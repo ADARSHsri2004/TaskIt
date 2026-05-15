4d02c859f3529d5d41ea8e2f168e0bc7ad0f5a45
ADARSHsri2004 <adarshsri619@gmail.com>
2026-05-15 20:14:48 +0530

    dockerization+prisma setup+middleware and utilities setup

---
+++ server/src/app.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
+import notFound from "./middlewares/notFound.middleware";
+import errorHandler from "./middlewares/error.middleware";
+
+const app = express();
+
+app.use(cors());
+
+app.use(helmet());
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "TaskIt API Running"
+  });
+});
+
+app.use(notFound);
+
+app.use(errorHandler);
+
+export default app;
